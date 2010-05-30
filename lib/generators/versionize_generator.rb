d34e0cbcc5bca5a404f7b4aba661839cb0c563e5
Henning Perl <perl@fast-sicher.de>
2010-05-30 15:21:07 +0200

    Fixed generator

--- lib/generators/versionize_generator.rb
+++ lib/generators/versionize_generator.rb
@@ -4,11 +4,31 @@ class VersionizeGenerator < Rails::Generators::Base
   desc "Create a migration to add a version table for your model."
   
   argument :model_name, :required => true, :type => :string,
-           :desc => "The model do versionize.", :banner => "ModelName"
+           :desc => "The model to versionize.", :banner => "ModelName"
   
-   def self.source_root
-     @source_root ||= File.expand_path('../templates', __FILE__)
-   end
+  def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
+
+  def model
+    begin
+      @model ||= @model_name.constantize
+    rescue NameError
+      raise Rails::Generators::Error.new("Could not find model '#{@model_name}'")
+    end
+  end
+  
+  def versions_table
+    @model_name.downcase + '_versions'
+  end
+
+  def columns
+    begin
+      @columns ||= model.content_columns.reject { |c| c.type == :datetime }
+    rescue Exception
+      raise Rails::Generators::Error.new("Could not find table for '#{@model}' in your database. Do you have pending migrations?")
+    end
+  end
   
   def generate_migration
     migration_template "migration.rb.erb", "db/migrate/#{migration_name}.rb"
